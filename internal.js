ace583bdf56ea36c7ddc1040cc10d39920807b49
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-06-29 15:58:45 +0530

    update

--- internal.js
+++ internal.js
@@ -54,3 +54,13 @@ document.querySelectorAll('.playAudio').forEach(function(anchor) {
 //   textInput.forEach(input => input.value= "");
 // })
 
+// SHEET CONTACT FORM
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyzGu3sdr8tbyvYkJSJJlAJNJu-rp1Bf8-2vwGY7etKLcH8ZgzjMKV7B8V7OD-uvmRu/exec'
+const form = document.forms['google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .catch(error => console.error('Error!', error.message))
+})
