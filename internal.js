c57fe1db8da1d80348d21468a6d0cfefba7de509
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-07-09 22:37:25 +0530

    fix index

--- internal.js
+++ internal.js
@@ -1,54 +1,50 @@
 function navigateTo(event, sectionId) {
-    event.preventDefault();
-    const sections = document.querySelectorAll(".content");
-    sections.forEach((section) => {
-      section.style.display = "none";
-    });
-    document.getElementById(sectionId).style.display = "block";
-
-    const links = document.querySelectorAll(".sidebar a");
-    links.forEach((link) => {
-      link.classList.remove("current");
-    });
-    event.target.classList.add("current");
-  }
-
-
-  const inputs = document.querySelectorAll("input, textarea");
-  const keySounds = [
-    new Audio("audio/static_audio_keyboard_key_1.mp3"),
-    new Audio("audio/static_audio_keyboard_key_2.mp3"),
-    new Audio("audio/static_audio_keyboard_key_3.mp3"),
-    new Audio("audio/static_audio_keyboard_key_5.mp3"),
-    new Audio("audio/static_audio_keyboard_key_6.mp3"),
-  ];
-  inputs.forEach((input) => {
-    input.addEventListener("keydown", () => {
-      const randomIndex = Math.floor(Math.random() * keySounds.length);
-      keySounds[randomIndex].currentTime = 0;
-      keySounds[randomIndex].play();
-    });
+  event.preventDefault();
+  const sections = document.querySelectorAll(".content");
+  sections.forEach((section) => {
+    section.style.display = "none";
   });
+  document.getElementById(sectionId).style.display = "block";
 
+  const links = document.querySelectorAll(".sidebar a");
+  links.forEach((link) => {
+    link.classList.remove("current");
+  });
+  event.target.classList.add("current");
+}
+
+const inputs = document.querySelectorAll("input, textarea");
+const keySounds = [
+  new Audio("audio/static_audio_keyboard_key_1.mp3"),
+  new Audio("audio/static_audio_keyboard_key_2.mp3"),
+  new Audio("audio/static_audio_keyboard_key_3.mp3"),
+  new Audio("audio/static_audio_keyboard_key_5.mp3"),
+  new Audio("audio/static_audio_keyboard_key_6.mp3"),
+];
+inputs.forEach((input) => {
+  input.addEventListener("keydown", () => {
+    const randomIndex = Math.floor(Math.random() * keySounds.length);
+    keySounds[randomIndex].currentTime = 0;
+    keySounds[randomIndex].play();
+  });
+});
 
 //   SOUNDS
 
-document.querySelectorAll('.playAudio').forEach(function(anchor) {
-    anchor.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default anchor behavior
-        var audioSrc = anchor.getAttribute('data-audio');
-        var audioPlayer = document.getElementById('audioPlayer');
-        audioPlayer.src = audioSrc;
-        audioPlayer.play();
-        
-        // Navigate to the href after a slight delay to allow the audio to play
-        setTimeout(function() {
-            window.location.href = anchor.href;
-        }, 400); // Adjust the delay as needed
-    });
+document.querySelectorAll(".playAudio").forEach(function (anchor) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    var audioSrc = anchor.getAttribute("data-audio");
+    var audioPlayer = document.getElementById("audioPlayer");
+    audioPlayer.src = audioSrc;
+    audioPlayer.play();
+
+    // Navigate to the href after a slight delay to allow the audio to play
+    setTimeout(function () {
+      window.location.href = anchor.href;
+    }, 400); // Adjust the delay as needed
+  });
 });
-
-
 
 // let btn = document.getElementsByClassName('.btn');
 // let Inputs = document.getElementsByClassName('.txt');
@@ -56,8 +52,6 @@ document.querySelectorAll('.playAudio').forEach(function(anchor) {
 // btn.addEventListener('click', () => {
 //   Inputs.forEach( input => input.value = '');
 // });
-
-
 
 // SHEET CONTACT FORM
 // const scriptURL = 'https://script.google.com/macros/s/AKfycbyzGu3sdr8tbyvYkJSJJlAJNJu-rp1Bf8-2vwGY7etKLcH8ZgzjMKV7B8V7OD-uvmRu/exec'
@@ -70,20 +64,20 @@ document.querySelectorAll('.playAudio').forEach(function(anchor) {
 //     .catch(error => console.error('Error!', error.message))
 // });
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyzGu3sdr8tbyvYkJSJJlAJNJu-rp1Bf8-2vwGY7etKLcH8ZgzjMKV7B8V7OD-uvmRu/exec';
-const form = document.forms['google-sheet'];
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyzGu3sdr8tbyvYkJSJJlAJNJu-rp1Bf8-2vwGY7etKLcH8ZgzjMKV7B8V7OD-uvmRu/exec";
+const form = document.forms["google-sheet"];
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            alert("Thanks for contacting us! We will contact you soon.");
-            // Clear the form fields
-            document.getElementById('name').value = "";
-            document.getElementById('email').value = "";
-            document.getElementById('message').value = "";
-            document.getElementById('company').value = "";
-        })
-        .catch(error => console.error('Error!', error.message));
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      alert("Thanks for contacting us! We will contact you soon.");
+      // Clear the form fields
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("company").value = "";
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
-
